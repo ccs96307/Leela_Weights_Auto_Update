add23549a7f80cdff7d431da2067a704a802bbad
ccs96307 <ccs96307@gmail.com>
2019-11-09 22:11:38 +0800

    Add a new function to continue download

--- auto_update.py
+++ auto_update.py
@@ -1,15 +1,8 @@
 # -*- coding:utf-8 -*-
-import gzip
-import shutil
+import os
+import re
 import requests
 import time
-import re
-
-
-# Never use this function, in case for need
-# def gzip_target():
-#     with open(r'weights.txt', 'rb') as f_in, gzip.open('test.gz', 'wb') as f_out:
-#         shutil.copyfileobj(f_in, f_out)
 
 
 def get_the_web_content():
@@ -28,7 +21,6 @@ def downloader(url, version, datetime):
     response = requests.get(url, stream=True)
     chunk_size = 1024
     content_size = int(response.headers['content-length'])
-    #print(content_size)
 
     if response.status_code == 200:
         print('[File version]: ', version)
@@ -108,8 +100,43 @@ def auto_update():
         text = re.findall('/networks.+', result)[0]
         text = re.sub('\.gz.+', '.gz', text)
         url = 'https://zero.sjeng.org' + text
-        downloader(url, version, datetime)
+        # downloader(url, version, datetime)
+        continue_download(url)
         break
+
+
+def continue_download(url):
+    start = time.time()
+    content_size = int(requests.get(url, stream=True).headers['Content-Length'])
+
+    if os.path.exists('weights.gz'):
+        temp_size = os.path.getsize('weights.gz')
+    else:
+        temp_size = 0
+
+    print('Temp:', temp_size)
+    print('Total:', content_size)
+
+    headers = {'Range': 'bytes=%d-' % temp_size}
+    r = requests.get(url, stream=True, headers=headers)
+
+    # Continue
+    chunk_size = 1024
+
+    print('[File size]: %0.2f MB' % (content_size / chunk_size / 1024))
+    with open('weights.gz', 'wb') as file:
+        for data in r.iter_content(chunk_size=chunk_size):
+            file.write(data)
+            temp_size += len(data)
+
+            print('\r'+'[Download progress]:[%s%s]%.2f%%;' % ('█' * int(temp_size * 20 / content_size), ' ' * (20 - int(temp_size * 20 / content_size)), float(temp_size / content_size * 100)), end='')
+
+    end = time.time()
+    print('\n'+'Donwload finished!')
+    print('Download time:%.2f s' % (end-start))
+    # version_list_text = 'download date:' + str(time.localtime(time.time()).tm_year) + '-' + str(time.localtime(time.time()).tm_mon) + '-' + str(time.localtime(time.time()).tm_mday) + ' ' + str(time.localtime(time.time()).tm_hour) + ':' + str(time.localtime(time.time()).tm_min) + ':' + str(time.localtime(time.time()).tm_sec) + '------------------' + str(version) + ':' + datetime + ':' + url + '\n'
+    # open('download_version_list.txt', 'a+', encoding='utf-8').write(version_list_text)
+
 
 
 if __name__ == '__main__':
